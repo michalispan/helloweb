d26e20d05394a2e110071983b07c6a943cb35864
michael <michael@michael-PC>
2017-05-04 12:46:24 +0300

    Changed db to Derby Embedded, Web Server Tomcat

--- src/java/db/DBConnection.java
+++ src/java/db/DBConnection.java
@@ -19,21 +19,42 @@ public class DBConnection {
     
     private static Connection conn = null;
     
+    private static String createAimodotes = "create table AIMODOTES" +
+"	AM INTEGER not null primary key GENERATED ALWAYS AS IDENTITY (START WITH 10000, INCREMENT BY 1)," +
+"	LAST_NAME VARCHAR(30)," +
+"	NAME VARCHAR(30)," +
+"	ADDRESS VARCHAR(30)," +
+"	PHONE VARCHAR(15)," +
+"	BLOOD_TYPE VARCHAR(3))";
+    
+    private static String createProsfora = "create table PROSFORA" +
+"	PK_P INTEGER not null primary key GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)," +
+"	BLOOD_BOTTLE INTEGER," +
+"	DATE DATE," +
+"	THEIRS_BLOOD INTEGER," +
+"	AM INTEGER references aimodotes(am)," +
+"	SXOLIA VARCHAR(100))";
+    
 //    private static String driver = "org.apache.derby.jdbc.ClientDriver";
 //    private static String url = "jdbc:derby://localhost:1527/sample";
 //    private static String user = "app";
 //    private static String pwd = "app";
     
-    private static String driver = "com.mysql.jdbc.Driver";
-    private static String url = "jdbc:mysql://localhost:3306/bloodbank";
-    private static String user = "app";
-    private static String pwd = "app";
+    private static String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+    private static String url = "jdbc:derby:bloodbank;create=true";
+    
+//    private static String driver = "com.mysql.jdbc.Driver";
+//    private static String url = "jdbc:mysql://localhost:3306/bloodbank";
+//    private static String user = "app";
+//    private static String pwd = "app";
     
     public static Connection getConnection() {
         if (conn == null) {
             try {
                 Class.forName(driver);
-                conn = DriverManager.getConnection(url, user, pwd);
+                conn = DriverManager.getConnection(url);
+                conn.createStatement().execute(createAimodotes);
+                conn.createStatement().execute(createProsfora);
             } catch (ClassNotFoundException ex) {
                 Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
             } catch (SQLException ex) {
